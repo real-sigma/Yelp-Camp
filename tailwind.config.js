4720e66c520478044de41b37dd5f907f814d5e4e
real-sigma <121306709+real-sigma@users.noreply.github.com>
2023-05-09 20:48:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+     'Archivo': ['Archivo', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
